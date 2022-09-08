143db4d5eff68f259d4f412eea4e27db4014c3d3
akbasgunay <gunayakbas@outook.com>
2022-09-08 18:21:41 +0300

    fix

---
+++ docs/precache-manifest.2a1da5d467b7ef3892729ea7f6a45362.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b833d968213d4e1a79a9c9c51a363647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee59e0b681201740ffd8",
+    "url": "/static/css/main.00e58b76.chunk.css"
+  },
+  {
+    "revision": "cb764fab402c9e4248a6",
+    "url": "/static/js/2.b5f83343.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/static/js/2.b5f83343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee59e0b681201740ffd8",
+    "url": "/static/js/main.455fad72.chunk.js"
+  },
+  {
+    "revision": "5427338b2b04b2090112",
+    "url": "/static/js/runtime-main.a6eeff82.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "635f73aeafa1724dfceaadace39795bc",
+    "url": "/static/media/avokadio.635f73ae.png"
+  },
+  {
+    "revision": "3fc1475eab81146efc8a8d6e533a35dd",
+    "url": "/static/media/deepsport.3fc1475e.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "49ffe76ab8ef0ed0aa739409c53bb276",
+    "url": "/static/media/gunay.49ffe76a.jpg"
+  },
+  {
+    "revision": "f163d18aabc3c6ab04235b08641ab5c1",
+    "url": "/static/media/gunayy.f163d18a.jpg"
+  },
+  {
+    "revision": "97cfd1d1d56ccc82875f840dcc23f05b",
+    "url": "/static/media/ibm.97cfd1d1.png"
+  },
+  {
+    "revision": "b1c07c925ccce0f326b99e2924f9312f",
+    "url": "/static/media/itü.b1c07c92.png"
+  },
+  {
+    "revision": "f753d3b4c7083e67494e79b8d82c1233",
+    "url": "/static/media/itü.f753d3b4.jpeg"
+  },
+  {
+    "revision": "2209785b9f654b33964f56213fccf222",
+    "url": "/static/media/matlab.2209785b.png"
+  },
+  {
+    "revision": "c59d354b3c003fd75b3a3dbb5dd425ef",
+    "url": "/static/media/sal.c59d354b.png"
+  },
+  {
+    "revision": "d30df2e5930c14548b502b7f65e4091d",
+    "url": "/static/media/vrrobotics.d30df2e5.png"
+  }
+]);
